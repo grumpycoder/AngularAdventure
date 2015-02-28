11bcd5dd9cbc60c972037ccb5ee4a511d6557fb1
grumpycoder <marklawrence72@gmail.com>
2015-02-28 13:33:28 -0600

    Added repository and breeze controller.

---
+++ AngularAdventure.DataAccess/AdventureWorksRepository.cs
@@ -0,0 +1,60 @@
+﻿using AngularAdventure.Model;
+using Breeze.ContextProvider;
+using Breeze.ContextProvider.EF6;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace AngularAdventure.DataAccess
+{
+    public class AdventureWorksRepository
+    {
+        private readonly EFContextProvider<AdventureWorksDbContext>
+       _contextProvider = new EFContextProvider<AdventureWorksDbContext>();
+
+        private AdventureWorksDbContext Context { get { return _contextProvider.Context; } }
+
+        public string Metadata
+        {
+            get { return _contextProvider.Metadata(); }
+        }
+        public SaveResult SaveChanges(JObject saveBundle)
+        {
+            return _contextProvider.SaveChanges(saveBundle);
+        }
+
+        public IQueryable<Customer> Customers
+        {
+            get { return Context.Customer; }
+        }
+
+        public IQueryable<Product> Products
+        {
+            get { return Context.Product; }
+        }
+
+        public IQueryable<SalesOrderHeader> SalesOrder
+        {
+            get { return Context.SalesOrderHeader; }
+        }
+
+        public IQueryable<ProductModel> ProductModels
+        {
+            get { return Context.ProductModel; }
+        }
+
+        public IQueryable<ProductDescription> ProductDescriptions
+        {
+            get { return Context.ProductDescription; }
+        }
+
+
+        public IQueryable<ProductCategory> ProductCategories
+        {
+            get { return Context.ProductCategory; }
+        }
+
+
+
+
+    }
+}
---
+++ AngularAdventure.Web/Controllers/BreezeController.cs
@@ -0,0 +1,79 @@
+﻿using AngularAdventure.DataAccess;
+using AngularAdventure.Model;
+using Breeze.ContextProvider;
+using Breeze.WebApi2;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Web.Http;
+
+namespace MediaCatalog.Web.Controllers
+{
+    [BreezeController]
+    public class BreezeController : ApiController
+    {
+
+        public BreezeController()
+        {
+
+        }
+
+        // Todo: inject via an interface rather than "new" the concrete class
+        readonly AdventureWorksRepository repository = new AdventureWorksRepository();
+
+        [HttpGet]
+        public string Metadata()
+        {
+            return repository.Metadata;
+        }
+
+        [HttpPost]
+        public SaveResult SaveChanges(JObject saveBundle)
+        {
+            return repository.SaveChanges(saveBundle);
+        }
+
+        [HttpGet]
+        public IQueryable<Customer> Customers()
+        {
+            return repository.Customers;
+        }
+
+        [HttpGet]
+        public IQueryable<Product> Products()
+        {
+            return repository.Products;
+        }
+
+        [HttpGet]
+        public IQueryable<SalesOrderHeader> SalesOrder()
+        {
+            return repository.SalesOrder;
+        }
+
+        /// <summary>
+        /// Query returing a 1-element array with a lookups object whose 
+        /// properties are all Rooms, Tracks, and TimeSlots.
+        /// </summary>
+        /// <returns>
+        /// Returns one object, not an IQueryable, 
+        /// whose properties are "rooms", "tracks", "timeslots".
+        /// The items arrive as arrays.
+        /// </returns>
+        [HttpGet]
+        public object Lookups()
+        {
+            var categories = repository.ProductCategories;
+            var models = repository.ProductModels;
+            var descriptions = repository.ProductDescriptions;
+
+            return new { categories, models, descriptions };
+        }
+
+        // Diagnostic
+        [HttpGet]
+        public string Ping()
+        {
+            return "pong";
+        }
+    }
+}
